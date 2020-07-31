9dc63505cb8f0b953f06f61961bbafd77eb0d78e
Amber Roberts Graham <robertsamb@gmail.com>
2020-07-31 11:04:00 -0500

    Add comments about variable order for script

--- middle.sh
+++ middle.sh
@@ -1,4 +1,8 @@
 # Add ability to set line numbers
 # Add ability to run on any file
 
+# $1 is filename
+# $2 is number of lines to send to head
+# $3 is number of lines to send to tail 
+
 head -n "$2" "$1" | tail -n "$3"
